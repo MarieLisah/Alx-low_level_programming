71fd2c9684fffbfbc0c2455ca4e7c69352ae5687
MarieLisah <lisaw.maria@gmail.com>
2022-09-08 15:38:07 -0700

    Oh my days

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <studio.h>
-
-/ *
-*
+/**
 *main - Entry Point
 *
 * Return :Always 0 (successful)
-* /
+*/
 int main(void)
 {
-> char message[] ="\"Programming is like building a multilingual puzzle":
-> puts(message):
-> return (0)
+char message[] = "\"Programming is like building a multilingual puzzle";
+puts(message);
+return (0);
 }
