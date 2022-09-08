6d59759b9f7e7e5c07abf5da6824a3109cf096a5
MarieLisah <lisaw.maria@gmail.com>
2022-09-08 15:19:17 -0700

    Oh my days

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+/ *
+*
+*main - Entry Point
+*
+* Return :Always 0 (successful)
+* /
+int main(void)
+{
+> char message[] ="\"Programming is like building a multilingual puzzle":
+> puts(message):
+> return (0)
+}
