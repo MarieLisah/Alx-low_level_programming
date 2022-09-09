b704bbc2f0c142309561f7951bd86dca4d94ce3c
MarieLisah <lisaw.maria@gmail.com>
2022-09-09 03:17:41 +0300

    Goodnight

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	char message[] = "\"Programming is like building a multilingual puzzle";
-	puts(message);
-	return (0);
+{char message[] = "\"Programming is like building a multilingual puzzle";
+
+puts(message);
+return (0);
 }
